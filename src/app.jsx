b1218242345af7fa0bdc1162385d97244b8dc671
Andrey_Tkachuk <tavideon@gmail.com>
2024-03-14 07:27:24 +0100

    pages

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter } from 'react-router-dom'
-import MainPage from "./pages/MainPage/MainPage";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import ContactPage from "./pages/ContactPage/ContactPage";
+import MainPage from "./pages/MainPage";
+import ErrorPage from "./pages/ErrorPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
     return (
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from 'pagestyles.module.scss'
 
 const ContactPage = () => {
   return (
